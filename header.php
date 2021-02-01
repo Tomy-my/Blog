8f33fe9f71e5514d401bead6413564190c21058c
Tomy-my <azerouto@gmail.com>
2021-02-01 16:19:15 +0100

    first commit

---
+++ header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="css/fonts.css" />
+        <link rel="stylesheet" href="css/styles.css" />
+        <link rel="stylesheet" href="css/resolutions.css" />
+        <link rel="icon" href="images/favicon.png" />
+        <title>intégration php</title>
+    </head>
+        <body>
+            
+            <div class="navbar">
+                <div class="logo">TOMY<br> Photographie</div>
+                <a href="#" class="home">Home</a>
+                <div class="photo">
+                    <a href="#" class="photo">Photographies</a>
+                <div class="liste">
+                    <ul>
+                    <a href="#"><li>Paysages</li></a>
+                    <a href="#"><li>Voitures</li></a>
+                    <a href="#"><li>En création</li></a>
+                    <a href="#"><li>En création</li></a>
+                    <a href="#"><li>En création</li></a>
+                    <a href="#"><li>En création</li></a>
+                    </ul>
+                </div>
+                </div>
+                <a href="#" class="contact">Me contacter</a>
+            </div>
+
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php include("header.php"); ?>
+
+<div class="acceuil">
+    <h2>Welcome to</h2>
+    <br>
+    <h1>My Photography Blog </h1>
+</div>
+
+
+
+
+
+
+
+
+    </body>
+</html>
